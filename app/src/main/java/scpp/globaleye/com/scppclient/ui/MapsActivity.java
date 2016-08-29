b08aaaa911f4b6982bcf6f7c4c600ebb79e718b1
umayanga123 <uma.madhushan@outlook.com>
2016-08-29 16:58:41 +0530

    make_map_loading_errer__16_8_29_16_58

--- app/src/main/java/scpp/globaleye/com/scppclient/ui/MapsActivity.java
+++ app/src/main/java/scpp/globaleye/com/scppclient/ui/MapsActivity.java
@@ -1,12 +1,14 @@
 package scpp.globaleye.com.scppclient.ui;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,6 +19,7 @@ import android.widget.Toast;
 
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
 
 
@@ -37,16 +40,17 @@ public class MapsActivity extends FragmentActivity implements LocationListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        //System.out.println("on createteeeeeeeeeeeeeee");
 
-        /*SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        setContentView(R.layout.activity_maps);
+       //System.out.println("on createteeeeeeeeeeeeeee");
+
+        SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
         googleMap = fm.getMap();
         getLocation();
 
         //////button
-        start_btn = (Button) findViewById(R.id.button1);
+        /*start_btn = (Button) findViewById(R.id.button1);
         stop_btn = (Button) findViewById(R.id.button2);
         stop_btn.setEnabled(false);
 
@@ -164,7 +168,8 @@ public class MapsActivity extends FragmentActivity implements LocationListener {
     @Override
     public void onBackPressed() {
           super.onBackPressed();
-          MapsActivity.this.finish();
-          this.overridePendingTransition(R.anim.stay_in, R.anim.bottom_out);
-    }
+            Intent intent = new Intent(MapsActivity.this, Services_View.class);
+            MapsActivity.this.startActivity(intent);
+            MapsActivity.this.finish();
+        }
 }
